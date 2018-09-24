880be6a692340a2f05bc3b07250a3ae8528f4184
Vadim Denisov <gidsor@gmail.com>
2018-09-24 10:45:03 +0500

    Initial Commit

---
+++ ARShootBugs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARShootBugs
+//
+//  Created by Vadim Denisov on 24.09.2018.
+//  Copyright © 2018 Vadim Denisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
