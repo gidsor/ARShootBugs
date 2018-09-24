af371cd042fb0d788210afe82ed226db62170e3b
Vadim Denisov <gidsor@gmail.com>
2018-09-24 11:25:24 +0500

    Change SKView to ARSKView and refactoring

--- ARShootBugs/GameViewController.swift
+++ ARShootBugs/GameViewController.swift
@@ -6,48 +6,48 @@
 //  Copyright © 2018 Vadim Denisov. All rights reserved.
 //
 
-import UIKit
-import SpriteKit
-import GameplayKit
+import ARKit
 
 class GameViewController: UIViewController {
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    var sceneView: ARSKView!
     
-    if let view = self.view as! SKView? {
-      // Load the SKScene from 'GameScene.sks'
-      if let scene = SKScene(fileNamed: "GameScene") {
-        // Set the scale mode to scale to fit the window
-        scene.scaleMode = .aspectFill
-        
-        view.presentScene(scene)
-      }
-      
-      view.ignoresSiblingOrder = true
-      
-      view.showsFPS = true
-      view.showsNodeCount = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.view as? ARSKView {
+            sceneView = view
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                view.presentScene(scene)
+            }
+
+            view.ignoresSiblingOrder = true
+
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
     }
-  }
-  
-  override var shouldAutorotate: Bool {
-    return true
-  }
-  
-  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-    if UIDevice.current.userInterfaceIdiom == .phone {
-      return .allButUpsideDown
-    } else {
-      return .all
+
+    override var shouldAutorotate: Bool {
+        return true
     }
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-  
-  override var prefersStatusBarHidden: Bool {
-    return true
-  }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
--- ARShootBugs/Types.swift
+++ ARShootBugs/Types.swift
@@ -9,11 +9,11 @@
 import SpriteKit
 
 enum Sounds {
-  static let fire =     SKAction.playSoundFileNamed("sprayBug", waitForCompletion: false)
-  static let hit =      SKAction.playSoundFileNamed("hitBug", waitForCompletion: false)
-  static let bugspray = SKAction.playSoundFileNamed("catchBugspray", waitForCompletion: false)
-  static let win =      SKAction.playSoundFileNamed("win.wav", waitForCompletion: false)
-  static let lose =     SKAction.playSoundFileNamed("lose.wav", waitForCompletion: false)
+    static let fire =     SKAction.playSoundFileNamed("sprayBug", waitForCompletion: false)
+    static let hit =      SKAction.playSoundFileNamed("hitBug", waitForCompletion: false)
+    static let bugspray = SKAction.playSoundFileNamed("catchBugspray", waitForCompletion: false)
+    static let win =      SKAction.playSoundFileNamed("win.wav", waitForCompletion: false)
+    static let lose =     SKAction.playSoundFileNamed("lose.wav", waitForCompletion: false)
 }
 
 
