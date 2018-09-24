a74af3c50720aad7d41bf4511e016dbde809fcbe
Vadim Denisov <gidsor@gmail.com>
2018-09-24 11:00:19 +0500

    Added template for game

---
+++ ARShootBugs/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  ARShootBugs
+//
+//  Created by Vadim Denisov on 24.09.2018.
+//  Copyright © 2018 Vadim Denisov. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let view = self.view as! SKView? {
+      // Load the SKScene from 'GameScene.sks'
+      if let scene = SKScene(fileNamed: "GameScene") {
+        // Set the scale mode to scale to fit the window
+        scene.scaleMode = .aspectFill
+        
+        view.presentScene(scene)
+      }
+      
+      view.ignoresSiblingOrder = true
+      
+      view.showsFPS = true
+      view.showsNodeCount = true
+    }
+  }
+  
+  override var shouldAutorotate: Bool {
+    return true
+  }
+  
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    if UIDevice.current.userInterfaceIdiom == .phone {
+      return .allButUpsideDown
+    } else {
+      return .all
+    }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+}
---
+++ ARShootBugs/Types.swift
@@ -0,0 +1,19 @@
+//
+//  Types.swift
+//  ARShootBugs
+//
+//  Created by Vadim Denisov on 24.09.2018.
+//  Copyright © 2018 Vadim Denisov. All rights reserved.
+//
+
+import SpriteKit
+
+enum Sounds {
+  static let fire =     SKAction.playSoundFileNamed("sprayBug", waitForCompletion: false)
+  static let hit =      SKAction.playSoundFileNamed("hitBug", waitForCompletion: false)
+  static let bugspray = SKAction.playSoundFileNamed("catchBugspray", waitForCompletion: false)
+  static let win =      SKAction.playSoundFileNamed("win.wav", waitForCompletion: false)
+  static let lose =     SKAction.playSoundFileNamed("lose.wav", waitForCompletion: false)
+}
+
+
